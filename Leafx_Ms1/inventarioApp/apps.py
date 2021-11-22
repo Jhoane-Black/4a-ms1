d49d4401e2757119f88c8ab3d6955271d437efdc
Jhoane-Black <54192804+Jhoane-Black@users.noreply.github.com>
2021-11-21 21:52:17 -0500

    Revert "Backend"

--- Leafx_Ms1/inventarioApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InventarioappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventarioApp'
--- Leafx_Ms1/inventarioApp/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cliente import *
-from .producto import *
-from .comentario import *
-from .proveedor import *
-from .resumen import *
